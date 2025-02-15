92f2959bc7478d8765ab2a9598116f0d9b4a9129
Matvei-Lise <89886662592@mail.ru>
2025-02-15 18:15:23 +0500

    изменил пути перехода меню

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-import "./script.js";
+/* import "./script.js";
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+ */
